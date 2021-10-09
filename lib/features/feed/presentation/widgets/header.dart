de5e3535ee063f2fbfc6b7a69b5a56216b7e0121
Gabriel Pigatto <gabrielpigatto@live.com>
2021-10-09 08:08:58 -0300

    feat:floating adj: header adj: body_component

--- lib/features/feed/presentation/widgets/header.dart
+++ lib/features/feed/presentation/widgets/header.dart
@@ -1,43 +1,31 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:jobsity_flutter_challenge/shared/svg_images.dart';
 
 class Header extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              FloatingActionButton(
-                onPressed: () {},
-                child: const Text('AH'),
-                backgroundColor: Theme.of(context).accentColor,
-                tooltip: 'AH',
-                mini: true,
-              ),
-              Container(
-                  height: 200,
-                  width: 200,
-                  child: SvgPicture.asset(SvgImages.jobsityLogo)),
-              ClipOval(
-                child: Material(
-                  child: InkWell(
-                    onTap: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: const Icon(Icons.search),
-                    ),
-                  ),
-                ),
-              )
-            ],
+    final _horizontalPadding = 60.0;
+    final _verticalPadding = 16.0;
+
+    final _horizontalAlign = CrossAxisAlignment.stretch;
+    final _verticalAlign = MainAxisAlignment.center;
+
+    final _logo = SvgImages.jobsityLogo;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: _horizontalPadding,
+        vertical: _verticalPadding,
+      ),
+      child: Row(
+        crossAxisAlignment: _horizontalAlign,
+        mainAxisAlignment: _verticalAlign,
+        children: [
+          Expanded(
+            child: SvgPicture.asset(_logo),
           ),
-        ),
+        ],
       ),
     );
   }
